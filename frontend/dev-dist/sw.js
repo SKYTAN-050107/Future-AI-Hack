9ab67bbfb778103ef851775bf74502391e70f082
Sky Tan <skycode112324@gmail.com>
2026-04-15 00:41:16 +0800

    feat: update .gitignore and add jsconfig.json for TypeScript configuration

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d70286d7'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.kvdpt4dm1ik"
+    "revision": "0.55mddh2t43g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
